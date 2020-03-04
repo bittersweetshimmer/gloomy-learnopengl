2bf582d7c73b0f89ed342117ed9985ffd1cf2246
Bittersweet Shimmer <bttrswt@protonmail.com>
2020-03-04 02:47:12 +0100

    Instancing example, not working.

--- gloomy_learnopengl/advanced_opengl/instancing/src/main.cpp
+++ gloomy_learnopengl/advanced_opengl/instancing/src/main.cpp
@@ -1,3 +1,97 @@
-int main(int argc, char* argv[]) {
-    return 0;
+#include <array>
+
+#include <gloomy/gloomy.hpp>
+#include <learnopengl/learnopengl.hpp>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+constexpr auto SCR_WIDTH = 800;
+constexpr auto SCR_HEIGHT = 600;
+
+namespace learnopengl {
+  inline void setup_camera(learnopengl::CameraContext& context, GLFWwindow& window);
+  inline void process_input(learnopengl::Camera& camera, float delta_time, GLFWwindow& window);
+}
+
+int main(int argc, char *argv[]) {
+  return learnopengl::glfw_context(SCR_WIDTH, SCR_HEIGHT, "gloomy", [&](GLFWwindow &window) {
+    gloomy::gl::enable(gloomy::Capability::DEPTH_TEST);
+
+    const auto program = gloomy::make_ready<gloomy::Program>(
+      gloomy::make_ready<gloomy::VertexShader>(learnopengl::load_vertex_shader("assets/shaders/VertexShader.hlsl").value()),
+      gloomy::make_ready<gloomy::FragmentShader>(learnopengl::load_fragment_shader("assets/shaders/FragmentShader.hlsl").value())
+    );
+    const auto planet = learnopengl::load_model("assets/models/planet/planet.obj").value();
+    const auto rock = learnopengl::load_model("assets/models/rock/rock.obj").value();
+
+    auto camera = learnopengl::Camera(glm::vec3(0.0f, 0.0f, 3.0f));
+    auto last_position = glm::vec2(SCR_WIDTH / 2.0f, SCR_HEIGHT / 2.0f);
+    bool first_mouse = true;
+    auto context = learnopengl::CameraContext{camera, last_position, first_mouse};
+
+    learnopengl::setup_camera(context, window);
+
+    auto delta_time = 0.0f;
+    auto last_frame = 0.0f;
+
+    learnopengl::loop(window, [&](double time) {
+      auto current_frame = static_cast<float>(time);
+      delta_time = current_frame - last_frame;
+      last_frame = current_frame;
+      
+      learnopengl::process_input(camera, delta_time, window);
+
+      gloomy::gl::clear_color(0.75f, 0.52f, 0.3f, 1.0f);
+      gloomy::gl::clear(gloomy::BufferBit::COLOR | gloomy::BufferBit::DEPTH);
+
+      glm::mat4 view = camera.get_view();
+      glm::mat4 projection = glm::perspective(glm::radians(camera.get_fov()), static_cast<float>(SCR_WIDTH) / static_cast<float>(SCR_HEIGHT), 0.1f, 100.0f);
+    });
+  });
+}
+
+#if defined(_WIN32) || defined(__CYGWIN__)
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,
+            int nCmdShow) {
+  return main(__argc, __argv);
+}
+#endif
+
+inline void learnopengl::setup_camera(learnopengl::CameraContext& context, GLFWwindow& window) {
+  glfwSetWindowUserPointer(&window, &context);
+
+  glfwSetInputMode(&window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);  
+  glfwSetCursorPosCallback(&window, [](GLFWwindow* window, double xpos, double ypos) {
+    auto context = static_cast<learnopengl::CameraContext*>(glfwGetWindowUserPointer(window));
+
+    if (context->first_mouse) {
+      context->last_position = glm::vec2(xpos, ypos);
+      context->first_mouse = false;
+    }
+
+    float xoffset = xpos - context->last_position.x;
+    float yoffset = context->last_position.y - ypos;
+    context->last_position = glm::vec2(xpos, ypos);
+
+    context->camera.process_mouse_movement(xoffset, yoffset);
+  });
+    
+  glfwSetScrollCallback(&window, [](GLFWwindow* window, double xoffset, double yoffset) {
+    auto context = static_cast<learnopengl::CameraContext*>(glfwGetWindowUserPointer(window));
+
+    context->camera.process_mouse_scroll(yoffset);
+  });
+}
+
+inline void learnopengl::process_input(learnopengl::Camera& camera, float delta_time, GLFWwindow& window) {
+  if (glfwGetKey(&window, GLFW_KEY_W) == GLFW_PRESS)
+    camera.process_keyboard(learnopengl::CameraMovement::FORWARD, delta_time);
+  else if (glfwGetKey(&window, GLFW_KEY_S) == GLFW_PRESS)
+    camera.process_keyboard(learnopengl::CameraMovement::BACKWARD, delta_time);
+  else if (glfwGetKey(&window, GLFW_KEY_A) == GLFW_PRESS)
+    camera.process_keyboard(learnopengl::CameraMovement::LEFT, delta_time);
+  else if (glfwGetKey(&window, GLFW_KEY_D) == GLFW_PRESS)
+    camera.process_keyboard(learnopengl::CameraMovement::RIGHT, delta_time);
 }
